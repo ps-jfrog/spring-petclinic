e373b91a06b825f7e94327fdb77830a6b922e493
Krishna <krishna@dayone.dev>
2026-08-12 10:59:48 -0700

    traffic manager report

---
+++ jfrog/jfcli-apptrust2app4vc.sh
@@ -0,0 +1,54 @@
+
+clear
+
+export JF_NAME="psazuse" JF_EDGE_NAME="psazeuwedge" JFROG_CLI_LOG_LEVEL="DEBUG" 
+export JF_RT_URL="https://${JF_NAME}.jfrog.io" PROJECT_KEY="ps-apptrust-rlm" 
+export TIMESTAMP="$(date '+%Y.%m.%d+%H%M')" 
+export APPLICATION_KEY="multi-apps" APPLICATION_VERSION="${TIMESTAMP}"
+jf config use ${JF_NAME}
+
+
+# ref: https://docs.jfrog.com/governance/docs/create-application-version-cli#example-2-create-an-application-version-using-a-spec-file
+echo "\n*** AppTrust: App Version create **\n"
+export AT_APP_SPEC_JSON="./at-app-spec.json"
+cat > "${AT_APP_SPEC_JSON}" <<EOF
+{
+  "builds": [
+    {
+      "name": "spring-petclinic",
+      "number": "2026.08.10+0952",
+      "repository_key": "${PROJECT_KEY}-build-info",
+      "include_dependencies": false
+    },{
+      "name": "spring-petclinic-rest",
+      "number": "2026.08.10+0952",
+      "repository_key": "${PROJECT_KEY}-build-info",
+      "include_dependencies": false
+    }
+  ]
+}
+EOF
+echo "AppTrust app spec file content: ${AT_APP_SPEC_JSON}"
+cat ${AT_APP_SPEC_JSON}
+
+# ref: https://docs.jfrog.com/governance/docs/create-application-version-cli 
+# jf apptrust version-create "app-spring-petclinic" 2026.07.23-1225 --spec="./at-app-spec.json" 
+jf apptrust version-create ${APPLICATION_KEY} ${APPLICATION_VERSION} --spec="${AT_APP_SPEC_JSON}" --tag="Package"
+
+# # # ref: https://docs.jfrog.com/governance/docs/promote-application-version-cli
+# echo "\n*** AppTrust: App Version promote to DEV **\n"
+# jf apptrust version-promote ${APPLICATION_KEY} ${APPLICATION_VERSION} DEV 
+# sleep 2
+
+# echo "\n*** AppTrust: App Version promote to QA **\n"
+# jf apptrust version-promote ${APPLICATION_KEY} ${APPLICATION_VERSION} QA
+# sleep 2
+
+# set -o xtrace # DEBUG ON
+# # ref: https://docs.jfrog.com/governance/docs/release-application-version-cli
+# echo "\n*** AppTrust: App Version promote to PROD **\n"
+# jf apptrust version-release ${APPLICATION_KEY} ${APPLICATION_VERSION}  
+
+echo "\n PROJECT_KEY: ${PROJECT_KEY}  APPLICATION_KEY: ${APPLICATION_KEY}  APPLICATION_VERSION: ${APPLICATION_VERSION} \n"
+rm -rf ${AT_APP_SPEC_JSON}
+jf -v
--- jfrog/jfcli-at-vc.sh
+++ jfrog/jfcli-at-vc.sh
@@ -1,3 +1,6 @@
+
+clear
+
 export JF_NAME="psazuse" JF_EDGE_NAME="psazeuwedge" JFROG_CLI_LOG_LEVEL="DEBUG" 
 export JF_RT_URL="https://${JF_NAME}.jfrog.io" PROJECT_KEY="ps-apptrust-rlm" APPLICATION_KEY="app-spring-petclinic"
 export RT_REPO_VIRTUAL="rbv2-spring-petclinic-mvn-virtual" # RT_REPO_LOCAL_DEFAULT="rbv2-spring-petclinic-mvn-init-local"
@@ -12,7 +15,7 @@ jf config use ${JF_NAME}
 echo " ** Maven package **"
 jf mvnc --global --repo-resolve-releases ${RT_REPO_VIRTUAL} --repo-resolve-snapshots ${RT_REPO_VIRTUAL}  \
   --repo-deploy-releases ${RT_REPO_VIRTUAL} --repo-deploy-snapshots ${RT_REPO_VIRTUAL} 
-jf mvn clean install -DskipTests=true -Denforcer.skip=true --build-name=${BUILD_NAME} --build-number=${BUILD_ID} \
+jf mvn clean install surefire-report:report -Denforcer.skip --build-name=${BUILD_NAME} --build-number=${BUILD_ID} \
  --project="${PROJECT_KEY}" --detailed-summary=true
 
 sleep 2
@@ -21,6 +24,33 @@ jf rt bag ${BUILD_NAME} ${BUILD_ID} --project="${PROJECT_KEY}"
 jf rt bce ${BUILD_NAME} ${BUILD_ID} --project="${PROJECT_KEY}"
 jf rt bp ${BUILD_NAME} ${BUILD_ID} --project="${PROJECT_KEY}"
 
+# ref: https://docs.jfrog.com/governance/docs/create-application-version-cli#example-2-create-an-application-version-using-a-spec-file
+echo "\n*** AppTrust: App Version create **\n"
+export AT_APP_SPEC_JSON="./at-app-spec.json"
+cat > "${AT_APP_SPEC_JSON}" <<EOF
+{
+  "builds": [
+    {
+      "name": "${BUILD_NAME}",
+      "number": "${BUILD_ID}",
+      "repository_key": "${PROJECT_KEY}-build-info",
+      "include_dependencies": false
+    }
+  ]
+}
+EOF
+echo "AppTrust app spec file content: ${AT_APP_SPEC_JSON}"
+cat ${AT_APP_SPEC_JSON}
+
+# ref: https://docs.jfrog.com/governance/docs/create-application-version-cli 
+# jf apptrust version-create "app-spring-petclinic" 2026.07.23-1225 --spec="./at-app-spec.json" 
+jf apptrust version-create ${APPLICATION_KEY} ${APPLICATION_VERSION} --spec="${AT_APP_SPEC_JSON}" --tag="Package"
+# jf apptrust version-create ${APPLICATION_KEY} ${APPLICATION_VERSION} --source-type-builds="name=${BUILD_NAME}, id=${BUILD_ID}, repository_key" --tag="prototype" --dry-run
+
+
+sleep 2
+rm -rf ${AT_APP_SPEC_JSON}
+rm -rf ${PREDICATE_JSON}
 
 echo "\n PROJECT_KEY: ${PROJECT_KEY}  BUILD_NAME: ${BUILD_NAME}   BUILD_ID: ${BUILD_ID}  APPLICATION_KEY: ${APPLICATION_KEY}  APPLICATION_VERSION: ${APPLICATION_VERSION} \n"
 jf -v
